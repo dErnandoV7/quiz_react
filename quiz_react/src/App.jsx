11c5a9d2a033f0b6b4db8e4055e385aa84ef3714
dErnandoV7 <d.ernandov7@gmail.com>
2024-02-01 08:57:06 -0300

    quiz em react part1
    componente welcome
    estilizando o componente welcome

---
+++ quiz_react/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz de programação</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
